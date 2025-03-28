15b84fe0ee3b6b49e8112c1095cdde620aba3cfa
AEva <crystar.z@proton.me>
2025-03-28 13:40:36 +0800

    feat(backend)!: :art: add predict router
    BREAKING CHANGE: use detect/con to detect websocket video frames

--- db/mysql/models.py
+++ db/mysql/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Float
 from sqlalchemy.orm import declarative_base, relationship
 from datetime import datetime, timezone
 
@@ -31,3 +31,21 @@ class Upload(Base):
 
     def __repe__(self):
         return f"<Upload(name={self.filename},path={self.filepath})>"
+
+
+class Detections(Base):
+    __tablename__ = "detections"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    class_name = Column(String(255), nullable=False)
+    confidence = Column(Float, nullable=False)
+    x_min = Column(Integer, nullable=False)
+    y_min = Column(Integer, nullable=False)
+    x_max = Column(Integer, nullable=False)
+    y_max = Column(Integer, nullable=False)
+    tag = Column(String(255), nullable=True)
+    num = Column(Integer, nullable=True)
+    upload_id = Column(Integer, ForeignKey("uploads.id", use_alter=True))
+
+    def __repe__(self):
+        return f"<Class(name={self.class_name},source={self.upload_id})>"
