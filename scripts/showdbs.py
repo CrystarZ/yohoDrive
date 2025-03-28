298a92c6f7957d83cf8bd3eedd3711b41aa52ab5
AEva <crystar.z@outlook.com>
2025-03-28 13:47:24 +0800

    feat(scripts/showdb.py): :sparkles: now show users,uploads,detections

--- scripts/showdbs.py
+++ scripts/showdbs.py
@@ -1,19 +1,40 @@
 import sys
 import os
 
-pwd = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-sys.path.append(pwd)
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from db.mysql.database import database as mysql
-from db.mysql.models import User
+from db.mysql.models import User, Upload, Detections
 from config import decoder as conf
 
+pwd = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 conf_db = conf(f"{pwd}/config.conf").Section("database").dict
 
-if __name__ == "__main__":
-    sql = mysql(**conf_db)
-    users = sql.query(User).all()
-    for user in users:
-        print(user)
 
+def showusers():
+    db = mysql(**conf_db)
+    E = db.query(User).all()
+    for e in E:
+        print(e)
+    db.close()
+
+
+def showuploads():
+    db = mysql(**conf_db)
+    E = db.query(Upload).all()
+    for e in E:
+        print(e)
+    db.close()
+
+
+def showdetections():
+    db = mysql(**conf_db)
+    E = db.query(Detections).all()
+    for e in E:
+        print(e)
+    db.close()
+
+
+if __name__ == "__main__":
+    showusers()
     exit(0)
