7628c62a43d2da77d38bbb36ccb84bd78a696160
AEva <crystar.z@outlook.com>
2025-05-26 02:20:14 +0800

    refactor(OCR.train.py): :recycle: refactor OCR/train.py same as DSIGN

--- OCR/train.py
+++ OCR/train.py
@@ -1,110 +1,171 @@
 import sys
 import os
+from typing import Tuple
+from tqdm import tqdm
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-from OCR.nets.LeNet5 import OCRNUM
+import cv2
 import torch
+from torch import Tensor
+from torch.nn.modules import Module
+from torch.optim.optimizer import Optimizer
+from torch.utils.data import DataLoader
+from torch.utils.data import Subset
 import torch.nn as nn
 import torch.utils.data as Data
 import torchvision
-from torchvision import transforms
-import cv2
+from torchvision.datasets import MNIST
+from torchvision import datasets, transforms
 
-cnn = OCRNUM()
-# print(cnn)
+from OCR.nets.LeNet5 import OCRNUM
+
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group["lr"]
+
+
+def fit_step(
+    model: Module,
+    optimizer: Optimizer,
+    lossfunc,
+    x: Tensor,  # images
+    y: Tensor,  # labels
+    train: bool = True,
+):
+    outputs = model(x)
+    loss_value = lossfunc(outputs, y)
+    if train:
+        optimizer.zero_grad()
+        loss_value.backward()
+        optimizer.step()
+
+    return loss_value
+
+
+def fit_epoch(
+    model: Module,
+    optimizer: Optimizer,
+    lossfunc,
+    traindata: DataLoader,
+    valdata: DataLoader,
+    device,
+) -> Tuple[float, float]:
+    loss = 0
+    valoss = 0
+    model = model.to(device)
+
+    model = model.train()
+    loop = tqdm(enumerate(traindata), total=len(traindata), desc="Train", ncols=100)
+    for step, (x, y) in loop:
+        x = x.to(device)
+        y = y.to(device)
+
+        loss_value = fit_step(model, optimizer, lossfunc, x, y)
+        loss += loss_value.item()
+
+        loop.set_postfix(
+            {"loss": f"{loss / (step + 1):.4f}", "lr": f"{get_lr(optimizer):.6f}"}
+        )
+
+    model = model.eval()
+    loop = tqdm(enumerate(valdata), total=len(valdata), desc="Eval ", ncols=100)
+    with torch.no_grad():
+        for step, (x, y) in loop:
+            x = x.to(device)
+            y = y.to(device)
+
+            loss_value = fit_step(model, optimizer, lossfunc, x, y, False)
+            valoss += loss_value.item()
+
+            loop.set_postfix(
+                {"loss": f"{valoss / (step + 1):.4f}", "lr": f"{get_lr(optimizer):.6f}"}
+            )
+
+    return loss, valoss
+
+
+def download():
+    DATASET_PATH = "./datasets"
+    MNIST(root=DATASET_PATH, download=True)
 
 
 def train():
+    LR = 1e-3
     EPOCH = 100
     BATCH_SIZE = 50
-    LR = 0.001  # 学习率
-    DOWNLOAD_MNIST = False  # 自动下载
+    DATASET_PATH = "./datasets"
+    SAVE_PATH = "./.output"
 
-    # (28,28)->(1,28,28)
     transform = transforms.ToTensor()
+    trainDataset = MNIST(root=DATASET_PATH, transform=transform, train=True)
+    valDataset = MNIST(root=DATASET_PATH, transform=transform, train=False)
+    valDataset = Subset(valDataset, indices=range(1000))
+    trainLoader = Data.DataLoader(dataset=trainDataset, batch_size=BATCH_SIZE)
+    valLoader = Data.DataLoader(dataset=valDataset, batch_size=2000, shuffle=False)
 
-    # data_set
-    trainDataset = torchvision.datasets.MNIST(
-        root="./data/", train=True, transform=transform, download=DOWNLOAD_MNIST
-    )
-    valDataset = torchvision.datasets.MNIST(
-        root="./data/", transform=transform, train=False
-    )
+    model = OCRNUM()
+    optimizer = torch.optim.Adam(model.parameters(), lr=LR)
+    lossfunc = nn.CrossEntropyLoss()  # 对应one-hot
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # 批训练 50个samples， 1  channel，28x28 (50,1,28,28)
-    train_loader = Data.DataLoader(
-        dataset=trainDataset,
-        batch_size=BATCH_SIZE,
-        shuffle=True,  # 打乱数据
-    )
-
-    # 进行测试
-    # 为节约时间，测试时只测试前2000个
-    val_loader = Data.DataLoader(dataset=valDataset, batch_size=2000, shuffle=False)
-    test_x, test_y = next(iter(val_loader))
-
-    # 训练
-    # 把x和y 都放入Variable中，然后放入cnn中计算output，最后再计算误差
-    optimizer = torch.optim.Adam(cnn.parameters(), lr=LR)
-    loss_func = nn.CrossEntropyLoss()  # 目标标签是one-hotted
-
-    # 开始训练
+    loss_history = []
+    valoss_history = []
     for epoch in range(EPOCH):
-        for step, (b_x, b_y) in enumerate(train_loader):
-            output = cnn(b_x)
-            loss = loss_func(output, b_y)  # 输出和真实标签的loss
-            optimizer.zero_grad()  # 清除梯度
-            loss.backward()
-            optimizer.step()
+        loss, valoss = fit_epoch(
+            model, optimizer, lossfunc, trainLoader, valLoader, device
+        )
 
-            if step % 50 == 0:
-                test_output = cnn(test_x)
-                pred_y = torch.max(test_output, 1)[1].data.numpy()
-                accuracy = float(
-                    (pred_y == test_y.data.numpy()).astype(int).sum()
-                ) / float(test_y.size(0))
-                print(
-                    "Epoch: ",
-                    epoch,
-                    "| train loss: %.4f" % loss.data.numpy(),
-                    "| test accuracy: %.2f" % accuracy,
-                )
+        loss = loss / len(trainLoader)
+        valoss = valoss / len(valLoader)
+        x, y = next(iter(valLoader))
+        pred_y = torch.max(model(x), 1)[1].data.numpy()
+        accuracy = (pred_y == y.data.numpy()).astype(int).sum() / y.size(0)
+        print(f"EPOCH: {epoch} | loss: {loss},valoss: {valoss}, accuracy: {accuracy}")
 
-        if epoch % 10 == 0:
-            torch.save(cnn.state_dict(), "./.output/cnn2.pth")  # 保存模型
+        loss_history.append(loss)
+        valoss_history.append(valoss)
+        if len(valoss_history) <= 1 or valoss <= min(valoss_history):
+            print("Save best model to best_weights.pth")
+            save_state_dict = model.state_dict()
+            torch.save(save_state_dict, f"{SAVE_PATH}/best_weights.pth")
+
+            with open(f"{SAVE_PATH}/loss.txt", "w", encoding="utf-8") as f:
+                for i in loss_history:
+                    f.write(str(i) + "\n")
+            with open(f"{SAVE_PATH}/val_loss.txt", "w", encoding="utf-8") as f:
+                for i in valoss_history:
+                    f.write(str(i) + "\n")
 
 
 def test():
     TESTNUM = 32
+    DATASET_PATH = "./datasets"
+    SAVE_PATH = "./.output"
 
     transform = transforms.ToTensor()
-    testDataset = torchvision.datasets.MNIST(
-        root="./data/", transform=transform, train=False
-    )
-    test_loader = Data.DataLoader(
-        dataset=testDataset, batch_size=TESTNUM, shuffle=False
-    )
-    test_x, test_y = next(iter(test_loader))
+    testDataset = MNIST(root=DATASET_PATH, transform=transform)
+    trainLoader = Data.DataLoader(dataset=testDataset, batch_size=TESTNUM)
 
-    cnn.load_state_dict(torch.load("./.output/cnn2.pth"))
-    cnn.eval()
-    # print 10 predictions from test data
-    inputs = test_x  # 测试32个数据
-    test_output = cnn(inputs)
-    pred_y = torch.max(test_output, 1)[1].data.numpy()
+    model = OCRNUM()
+    model.load_state_dict(torch.load(f"{SAVE_PATH}/best_weights.pth"))
+    model.eval()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # 显示结果
-    print(pred_y, "prediction number")  # 打印识别后的数字
-    print(test_y.numpy(), "real number")
+    x, y = next(iter(trainLoader))
+    x = x.to(device)
+    y = y.to(device)
 
-    img = torchvision.utils.make_grid(inputs)
+    pred_y = torch.max(model(x), 1)[1].data.numpy()
+    accuracy = (pred_y == y.data.numpy()).astype(int).sum() / y.size(0)
+
+    print("accuracy          :", accuracy)
+    print("prediction number :", pred_y)
+    print("real number       :", y.numpy())
+
+    img = torchvision.utils.make_grid(x)
     img = img.numpy().transpose(1, 2, 0)
 
-    # 下面三行为改变图片的亮度
-    # std = [0.5, 0.5, 0.5]
-    # mean = [0.5, 0.5, 0.5]
-    # img = img * std + mean
     cv2.imshow("win", img)
     cv2.waitKey(0)
 
