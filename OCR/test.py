41d9e9cf9df8519db72167e05577542ccd7afb56
AEva <crystar.z@outlook.com>
2025-05-26 02:54:07 +0800

    refactor(OCR.test.py): :art: refactor OCR/test.py

--- OCR/test.py
+++ OCR/test.py
@@ -2,14 +2,17 @@ import sys
 import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 from OCR.ocr import OCR, tomygray
 from PIL import Image
 
+WEIGHT_PATH = "./weights/tl_v8s.pth"
+TEST_PATH = "./img/OCR_test.jpg"
+CH = "R"
+
 if __name__ == "__main__":
-    img = Image.open("./img/test.jpg")
+    img = Image.open(TEST_PATH)
     img.show()
 
-    img = tomygray(img, "R")
-    ocr = OCR(module_path="./weights/cnn2.pth", cuda=False)
+    img = tomygray(img, CH)
+    ocr = OCR(module_path=WEIGHT_PATH)
     print(ocr.predict(img))
